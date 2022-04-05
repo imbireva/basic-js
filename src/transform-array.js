0f8611ecdcafaff8989d6ae8c850a3489a81a4b7
Daria Imbireva <imbireva.daria@gmail.com>
2022-04-05 14:00:04 +0300

    complete task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (arr instanceof Array) {
+    const copy = arr.slice();
+    for (let i = 0; i < copy.length; i++) {
+      switch (copy[i]) {
+        case '--discard-next':
+          i === (copy.length - 1) ? copy.pop() : copy.splice((i + 1), 1);
+          break;
+
+        case '--discard-prev':
+          i === 0 ? copy.shift() : copy.splice((i - 1), 2);
+          break;
+
+        case '--double-next':
+          i === (copy.length - 1) ? copy.pop() : copy[i] = copy[i + 1];
+          break;
+
+        case '--double-prev':
+          if (i === 0) {
+            copy.shift()
+          }
+          else {
+            copy[i - 1] === '--discard-next' ? copy.splice((i - 1), 2) : copy[i] = copy[i - 1];
+          }
+          break; 
+      }
+    }
+    copy.forEach((item, index) => {
+      if (item === '--discard-next') {
+        copy.splice(index, 1);
+      }
+    });
+    return copy;
+  } 
+  else {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
 }
 
 module.exports = {
